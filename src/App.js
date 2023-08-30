5566f29e94d8e667610996b4a2e513f948bd04d2
Aishwarya J <aishwarya9425@gmail.com>
2023-08-30 15:46:44 +0000

    children prop

--- src/App.js
+++ src/App.js
@@ -68,20 +68,29 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setIsOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setIsOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(num);
   }
   return (
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
@@ -89,7 +98,7 @@ function AccordionItem({ num, title, text }) {
       <p className="title"> {title}</p>
       <p className="icon">{isOpen ? "-" : "+"} </p>
 
-      {isOpen && <div className="content-box">{text} </div>}
+      {isOpen && <div className="content-box">{children} </div>}
     </div>
   );
 }
